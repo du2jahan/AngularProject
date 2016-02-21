cdedf7c18779d3f10d973fde8889361c8078ac16
du2jahan <du2jahan@gmail.com>
2016-02-21 00:37:00 -0500

    XXX000: Add Player Class

---
+++ TennisGame/src/main/tennis/Player.java
@@ -0,0 +1,38 @@
+package main.tennis;
+
+public class Player {
+	private String name;
+	private int score;
+
+	public Player(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public void playerScoreIncrement() {
+		score++;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [name=" + name + ", score=" + score + "]";
+	}
+
+}
